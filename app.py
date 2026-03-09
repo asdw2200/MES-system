460044233624cd0c6d6fcedff82dd130b58f02c0
asdw2200 <asdw2200@gmail.com>
2026-03-09 18:11:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def delete_incoming_data_multiple(sheet_row_indices):
     creds_dict = dict(st.secrets["gcp_service_account"])
     creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
     client = gspread.authorize(creds)
-    sheet_url = "https://docs.google.com/spreadsheets/d/1fh1XIF7Z1tlQQV7zFUql5gjv-veBgltjm0Hb2vflEo8" 
+    sheet_url = "https://docs.google.com/spreadsheets/d/1fh1XlF7Z1tlQQV7zFUql5gjv-veBgItjm0Hb2vfIEo8/edit?gid=1166124159#gid=1166124159" 
     doc = client.open_by_url(sheet_url)
     sheet_incoming = doc.worksheet("수입검사일지")
     
@@ -390,5 +390,6 @@ elif menu == "📥 수입자재 검사대기":
 
     else:
         st.success("✨ 현재 대기 중이거나 등록된 수입자재 내역이 없습니다.")
+
 
 
